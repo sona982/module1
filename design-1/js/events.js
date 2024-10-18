ebc45d7066ae94f54fee90aa15a98b801b0626e4
sona982 <nguyentuanphong982@gmail.com>
2024-10-18 20:33:42 +0700

    update

---
+++ design-1/js/events.js
@@ -0,0 +1,5 @@
+function changePage(pageNum) {
+    // Replace this with your logic to load new page content
+    console.log("Chuyển đến trang: " + pageNum);
+  }
+  
---
+++ design-2/js/mobile.js
@@ -0,0 +1,5 @@
+    document.getElementById('hamburger-btn').addEventListener('click', function() {
+        const menuItems = document.getElementById('menu-items');
+        menuItems.classList.toggle('hidden'); // Chuyển đổi lớp 'hidden' để hiển thị/ẩn menu
+    });
+
